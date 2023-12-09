3b92e62765322ad0a876bec30af35288e57c1ac9
hanion <55713249+hanion@users.noreply.github.com>
2023-12-09 21:00:32 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace efsane_oyun.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
